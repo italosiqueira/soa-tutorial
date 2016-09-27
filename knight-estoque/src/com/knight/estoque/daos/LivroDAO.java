f77f53bc57bf2749b05fd8cb005416e603223925
Italo Siqueira Lima <italosiqueira@gmail.com>
2016-09-27 06:20:58 -0300

    Initial commit. Basic SOA Web Service.

---
+++ knight-estoque/src/com/knight/estoque/daos/LivroDAO.java
@@ -0,0 +1,41 @@
+package com.knight.estoque.daos;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.knight.estoque.modelos.Livro;
+
+public class LivroDAO {
+
+	public List<Livro> listarLivros() {
+		
+		List<Livro> livros = new ArrayList<Livro>();
+		
+		livros.add(getLivroGuiaDoProgramador());
+		livros.add(getLivroRubyOnRails());
+		
+		return livros;
+	}
+
+	private Livro getLivroGuiaDoProgramador() {
+		Livro livro = new Livro();
+		livro.setAnoDePublicacao(2012);
+		livro.setAutores(Arrays.asList("Paulo Silveira", "Adriano Almeida"));
+		livro.setEditora("Casa do Código");
+		livro.setNome("Guia do Programador");
+		livro.setResumo("Vá do \"nunca programei\" a ...");
+		return livro;
+	}
+
+	private Livro getLivroRubyOnRails() {
+		Livro livro2 = new Livro();
+		livro2.setAnoDePublicacao(2012);
+		livro2.setAutores(Arrays.asList("Vinícius Baggio Fuentes"));
+		livro2.setEditora("Casa do Código");
+		livro2.setNome("Ruby On Rails");
+		livro2.setResumo("Crie rapidamente aplicações web");
+		return livro2;
+	}
+
+}
---
+++ knight-estoque/src/com/knight/estoque/modelos/Livro.java
@@ -0,0 +1,101 @@
+package com.knight.estoque.modelos;
+
+import java.util.List;
+
+public class Livro {
+
+	private String nome;
+
+	private List<String> autores;
+
+	private String editora;
+
+	private Integer anoDePublicacao;
+
+	private String resumo;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<String> getAutores() {
+		return autores;
+	}
+
+	public void setAutores(List<String> autores) {
+		this.autores = autores;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
+	public Integer getAnoDePublicacao() {
+		return anoDePublicacao;
+	}
+
+	public void setAnoDePublicacao(Integer anoDePublicacao) {
+		this.anoDePublicacao = anoDePublicacao;
+	}
+
+	public String getResumo() {
+		return resumo;
+	}
+
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((anoDePublicacao == null) ? 0 : anoDePublicacao.hashCode());
+		result = prime * result + ((autores == null) ? 0 : autores.hashCode());
+		result = prime * result + ((editora == null) ? 0 : editora.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		if (anoDePublicacao == null) {
+			if (other.anoDePublicacao != null)
+				return false;
+		} else if (!anoDePublicacao.equals(other.anoDePublicacao))
+			return false;
+		if (autores == null) {
+			if (other.autores != null)
+				return false;
+		} else if (!autores.equals(other.autores))
+			return false;
+		if (editora == null) {
+			if (other.editora != null)
+				return false;
+		} else if (!editora.equals(other.editora))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+}
