731bb9d38ec27b2cfd1cbf36a3da42831e626cce
Italo Siqueira Lima <italosiqueira@gmail.com>
2016-11-15 10:23:42 -0300

    Removed Usuario subsystem (and REST access) subsystem to a separate project.

--- knight-estoque/src/main/java/com/knight/estoque/modelos/Usuario.java
+++ knight-estoque/src/main/java/com/knight/estoque/modelos/Usuario.java
@@ -1,54 +1,13 @@
 package com.knight.estoque.modelos;
 
-import java.util.ArrayList;
-import java.util.Collection;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Transient;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
-
-import com.knight.estoque.modelos.rest.Link;
-import com.knight.estoque.modelos.rest.RESTEntity;
-
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
-@Entity
-public class Usuario extends EntidadeModelo implements RESTEntity {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+public class Usuario{
 
 	private String nome;
 
 	private String login;
 
 	private String senha;
-
-	@OneToOne(cascade = { CascadeType.ALL }, orphanRemoval = true)
-	@XmlTransient
-	private Imagem imagem;
-
-	@XmlElement(name = "link")
-	@Transient
-	private Collection<Link> links;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 
 	public String getNome() {
 		return nome;
@@ -74,28 +33,10 @@ public class Usuario extends EntidadeModelo implements RESTEntity {
 		this.senha = senha;
 	}
 
-	public Imagem getImagem() {
-		return imagem;
-	}
-
-	public void setImagem(Imagem imagem) {
-		this.imagem = imagem;
-	}
-
-	@Override
-	public void adicionarLink(Link link) {
-		if (links == null) {
-			links = new ArrayList<>();
-		}
-
-		links.add(link);
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((login == null) ? 0 : login.hashCode());
 		return result;
 	}
@@ -109,11 +50,6 @@ public class Usuario extends EntidadeModelo implements RESTEntity {
 		if (getClass() != obj.getClass())
 			return false;
 		Usuario other = (Usuario) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
 		if (login == null) {
 			if (other.login != null)
 				return false;
--- knight-estoque/src/main/java/com/knight/estoque/modelos/rest/RESTEntity.java
+++
@@ -1,7 +0,0 @@
-package com.knight.estoque.modelos.rest;
-
-public interface RESTEntity {
-	
-	public void adicionarLink(Link link);
-
-}
